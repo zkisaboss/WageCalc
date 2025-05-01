e683bd29b663eb3c5cab51cb37666c9a69c44e9f
Zach Kosove <51681731+zkisaboss@users.noreply.github.com>
2025-05-01 23:18:20 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+from bls import fetch_bls_data, process_bls_data
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    bls_data = fetch_bls_data()
+    items = process_bls_data(bls_data)
